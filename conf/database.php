50bb89e6d32b14613c931037de11591605e49867
Rean <lei.zhang12@yintech.cn>
2018-07-23 09:29:26 +0800

    优化

---
+++ conf/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'a' => [
+        'dsn' => 'mysql:host=127.0.0.1;port=3306;charset=utf8mb4',
+        'user' => 'root',
+        'password' => 'reaburoa'
+    ],
+    'b' => [
+        'dsn' => 'mysql:host=127.0.0.1;port=3306;charset=utf8mb4',
+        'user' => 'root',
+        'password' => 'reaburoa'
+    ],
+    'test' => 'a',
+];
