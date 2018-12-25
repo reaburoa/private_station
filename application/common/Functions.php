8d8d648efcd3c12bcbd5212e83a3f46c290032db
Rean <zl@sunmi.com>
2018-12-25 17:01:51 +0800

    公共方法

--- application/common/Functions.php
+++ application/common/Functions.php
@@ -5,14 +5,10 @@ use Yaf\Application;
 
 class Functions
 {
-    private static $package = null;
-
-    public static function T($code, $lang = 'zh_cn')
+    public static function test()
     {
-        $library = Application::app()->getConfig()->get("application")->library;
-        if (empty(self::$package)) {
-            self::$package = require_once($library . "/language/{$lang}.php");
-        }
-        return empty(self::$package[$code]) ? $code : self::$package[$code];
+        echo Application::app()->getConfig()->get('application')->directory;
+        echo "##";
+        echo "Common test function";
     }
 }
