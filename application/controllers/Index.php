a7a21254ee282825ac030df42daf4b0d878ffac7
Rean <zl@sunmi.com>
2020-06-21 17:43:03 +0800

    feat:yh

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -6,7 +6,6 @@ class IndexController extends HttpKernel
 {
     public function indexAction()
     {
-        \Common\Functions::test();
         $id = $this->get('id', 2);
         $this->renderTpl('index', ['id' => $id]);
     }
